d97ee05dd7b2198588d7ef8d8651076f48288ea1
yeng-lune <152809438+yeng-lune@users.noreply.github.com>
2023-12-04 08:54:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var setttingsmenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
+
+function settingsMenuToggle(){
+    setttingsmenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem("theme", "dark");
+    }
+    else{
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+
+if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme", "light");
+}
